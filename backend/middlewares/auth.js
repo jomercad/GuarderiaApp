da1bc58146a470db7f28471a7109f1b386788f8a
Jose <jose@MacBook-Pro-de-Jose.local>
2025-02-22 18:48:55 -0500

    Ajustes login

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,20 +3,25 @@ const { User } = require("../models");
 
 exports.authenticateJWT = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  
-  if (!authHeader?.startsWith('Bearer ')) {
+
+  if (!authHeader?.startsWith("Bearer ")) {
     return res.status(401).json({ error: "Token no proporcionado" });
   }
 
-  const token = authHeader.split(' ')[1];
-  
+  const token = authHeader.split(" ")[1];
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findByPk(decoded.userId);
-    
+
     if (!user) throw new Error("Usuario no encontrado");
-    
-    req.user = user;
+
+    // req.user = user;
+    req.user = {
+      id: user.id,
+      role: user.role,
+      parentId: user.parentId,
+    };
     next();
   } catch (error) {
     res.status(401).json({ error: "Token inválido o expirado" });
@@ -27,7 +32,7 @@ exports.authorizeRoles = (...allowedRoles) => {
   return (req, res, next) => {
     if (!allowedRoles.includes(req.user.role)) {
       return res.status(403).json({
-        error: `Acceso prohibido para el rol ${req.user.role}`
+        error: `Acceso prohibido para el rol ${req.user.role}`,
       });
     }
     next();
